863f12a6c4c99e1aa8a2df6052c5d13acde2bf82
Matt Lawson <bodhimindflow@gmail.com>
2024-10-17 01:21:38 +0900

    vercel-serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
 
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
